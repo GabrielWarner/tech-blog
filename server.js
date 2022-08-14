412cb8d83a043c55c5c1842c535eaa44a5c6bf69
GabrielWarner <gabe0412@uw.edu>
2022-08-14 12:54:20 -0700

    added initial files and installed packages, used boiler plate code for server and db

---
+++ server.js
@@ -0,0 +1,46 @@
+//Express packages
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+//routes and sequelize
+const allRoutes = require("./controllers");
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const sess = {
+    // secret: process.env.SESSION_SECRET,
+    secret: "secret",
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 2,
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+  };
+
+  app.use(session(sess));
+
+  // Static directory
+app.use(express.static("public"));
+
+//template engine
+const hbs = exphbs.create({});
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use("/", allRoutes);
+
+sequelize.sync({ force: false }).then(function () {
+    app.listen(PORT, function () {
+      console.log("App listening on PORT " + PORT);
+    });
+  });
